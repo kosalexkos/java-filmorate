715124512338c354f45e96c5b49ccc912f73bcbd
Alexandr <kosalexkos@yandex.ru>
2023-06-28 15:07:52 +0300

    proj with annotations
    added severak annotations to validate date

--- filmorate/src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,15 +1,14 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Builder;
 import lombok.Data;
+
 import java.time.LocalDate;
 
 @Data
-@Builder
 public class Film {
-    private int id;
-    private String name;
-    private String description;
-    private LocalDate releaseDate;
-    private int duration;
+    int id;
+    String name;
+    String description;
+    LocalDate releaseDate;
+    int duration;
 }
--- filmorate/src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,16 +1,15 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Builder;
 import lombok.Data;
+
 import java.time.LocalDate;
 
 @Data
-@Builder
 public class User {
-    private int id;
-    private String email;
-    private String login;
-    private String nickname;
-    private LocalDate birthday;
+    int id;
+    String email;
+    String login;
+    String nickname;
+    LocalDate birthday;
 }
 
